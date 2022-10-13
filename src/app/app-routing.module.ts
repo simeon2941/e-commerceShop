66d65fa031f0e84a2f3a0591da404c240690a7e4
Simeon Lico <simeon.lico@vizientinc.com>
2022-10-13 09:08:25 -0500

    addedUsersService

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,27 +5,36 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { ExternalApiComponent } from './pages/external-api/external-api.component';
 import { ErrorComponent } from './pages/error/error.component';
 import { AuthGuard } from '@auth0/auth0-angular';
+import {DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
   {
     path: 'profile',
     component: ProfileComponent,
     canActivate: [AuthGuard],
   },
+  { path: 'detail/:id', component: HeroDetailComponent },
   {
     path: 'external-api',
     component: ExternalApiComponent,
     canActivate: [AuthGuard],
   },
   {
+    path: 'dashboard',
+    component: DashboardComponent,
+
+  },
+  {
     path: 'error',
     component: ErrorComponent,
   },
-  {
-    path: '',
-    component: HomeComponent,
-    pathMatch: 'full',
-  },
+
 ];
 
 @NgModule({
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Hero } from './hero';
+import { User } from './users';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const heroes = [
-      { id: 12, name: 'Dr. Nice' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr. IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+    const users = [
+      { id: 12, name: 'Simeon', lastName: 'Admin', email: 'simeonlico@gmail.com', isAdmin: true},
+      { id: 13, name: 'Juho', lastName: 'Makela', email: 'juho.makela@example.com', isAdmin: false},
+      { id: 11, name: 'Eckard', lastName: 'Zoller', email: 'eckard.zoller@example.com', isAdmin: false},
+      { id: 19, name: 'Perfeito', lastName: 'Moraes', email: 'perfeito.moraes@example.com', isAdmin: false},
+      { id: 23, name: 'Ayub', lastName: 'Resell', email: 'ayub.resell@example.com', isAdmin: false},
+      { id: 32, name: 'Leticia', lastName: 'Luevano', email: 'leticia.luevano@example.com', isAdmin: false},
+      { id: 33, name: 'Eren', lastName: 'Karaduman', email: 'eren.karaduman@example.com', isAdmin: false},
+      { id: 43, name: 'Jessica', lastName: 'Kim', email: 'jessica.kim@example.com', isAdmin: false},
+      { id: 31, name: 'Maxwell', lastName: 'Wright', email: 'maxwell.wright@example.com', isAdmin: false},
+      { id: 1, name: 'Ismael', lastName: 'Delgado', email: 'ismael.delgado@example.com', isAdmin: false},
     ];
-    return {heroes};
+    return {users};
   }
 
   // Overrides the genId method to ensure that a hero always has an id.
@@ -26,7 +28,7 @@ export class InMemoryDataService implements InMemoryDbService {
   // the method below returns the initial number (11).
   // if the heroes array is not empty, the method below returns the highest
   // hero id + 1.
-  genId(heroes: Hero[]): number {
-    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  genId(users: User[]): number {
+    return users.length > 0 ? Math.max(...users.map(hero => hero.id)) + 1 : 11;
   }
 }
