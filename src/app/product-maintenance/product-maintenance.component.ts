963bc029aedeec2d89d6251f0b75d2054d4c583e
simeon2941 <simeonlico@gmail.com>
2023-01-19 14:20:16 -0600

    product-maintenance component

--- src/app/product-maintenance/product-maintenance.component.ts
+++ src/app/product-maintenance/product-maintenance.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Product } from '../products';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-product-maintenance',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-maintenance.component.css']
 })
 export class ProductMaintenanceComponent implements OnInit {
+  product: Product;
 
-  constructor() { }
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getProduct();
   }
+  getProduct(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.productService.getProduct(id).subscribe(product => (this.product = product));
+  }
+  saveProduct(form): void {
+    if(this.product){
+      this.productService.updateProduct(this.product).subscribe(() => {this.goBack()
+      });
+    }
 
+  }
+  goBack(): void {
+    this.location.back();
+  }
 }
